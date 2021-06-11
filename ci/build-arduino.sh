c5ead7f69e6f86e34fe198b5ecfb7316a24471fa
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-06-11 19:42:57 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -11,8 +11,9 @@ mkdir $HOME/Arduino
 mkdir $HOME/Arduino/libraries
 # Install Arduino IDE
 export PATH="$GITHUB_WORKSPACE/bin:&PATH"
-export PATH="/home/vsts/bin:&PATH"
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+cd $GITHUB_WORKSPACE/bin
+ls -l
 arduino-cli config init
 arduino-cli core update-index
 
