3497f7740367b08505644f57b748aedcb1fb0430
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-06-14 12:51:43 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -27,7 +27,8 @@ echo "list files"
 ls
 # Compile all *.ino files for the Arduino Uno
 for f in **/*.ino ; do
-  arduino-cli compile -b arduino:avr:uno $f
-done
-echo "Printing pwd"
-pwd
+  arduino-cli compile -b arduino:avr:uno --export-binaries $PWD/vsts.ino
+echo "listing files---"
+ls -Rlh
+#promini_blink.ino
+#done
