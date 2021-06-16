42d5d7f89129fdc808b93e8f0a60a12816c78991
iamashraf21 <iamashraf21@gmail.com>
2021-06-16 19:06:26 +0530

    added the micro-ecc lib in the .ino file

--- arduino_sketch_devops.ino
+++ arduino_sketch_devops.ino
@@ -1,3 +1,4 @@
+#include <uECC.h>
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode (13, OUTPUT);
@@ -6,6 +7,9 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+
+  const struct uECC_Curve_t * curve = uECC_secp256k1();
+  
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(100);                       // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
