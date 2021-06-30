8d38b02402e75e357d85d3b6d450feb3bbdda540
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-06-30 18:19:17 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -13,7 +13,7 @@ ls
 echo "listing files---"
 ls -Rlh
 pwd
-cd $(Build.SourcesDirectory)/arduino_sketch_devops/ci
+cd $HOME/Documents/myagent/_work/2/s/arduino_sketch_devops/ci
 arduino-cli compile -b Seeeduino:samd:zero -e
 ls -l
 #arduino_sketch_devops.ino
