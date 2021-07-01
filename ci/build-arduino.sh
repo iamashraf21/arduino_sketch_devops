877b11a4f9bd4820a6873adf2b5d3e799cea5cd0
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-01 09:56:48 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -6,15 +6,7 @@ set -e
 shopt -s globstar
 # Make sure we are inside the github workspace
 cd $GITHUB_WORKSPACE
-echo "Printing pwd"
-pwd
-echo "list files"
-ls
-echo "listing files---"
-ls -Rlh
 pwd
 cd $HOME/Documents/myagent/_work/2/s/arduino_sketch_devops
 arduino-cli compile -b Seeeduino:samd:zero -e
-ls -l
-#arduino_sketch_devops.ino
-#done
+
