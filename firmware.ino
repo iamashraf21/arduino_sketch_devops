79ef858f65476a60e8594c78995e5fa62c6d3ffa
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-06 09:22:59 +0530

    Update firmware.ino

--- firmware.ino
+++ firmware.ino
@@ -14,5 +14,5 @@ void loop() {
   delay(100);                       // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(100);                       // wait for a second
-  Serial.println("LED loop");
+  Serial.println("LED LOOP");
 }
