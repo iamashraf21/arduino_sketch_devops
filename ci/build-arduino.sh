bd349b5f94d74eb4b20ff22e908d3f548fd9d1e0
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-06 15:46:41 +0530

    Update build-arduino.sh

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -10,3 +10,4 @@ arduino-cli compile -b Seeeduino:samd:zero -e
 cd build/Seeeduino.samd.zero/
 mkdir ../../build_signed
 python ../../ci/sign.py ../../ci/private.pem firmware.ino.bin 1 --out ../../build_signed/firmware.signed 
+python ../../ci/verify.py ../../ci/public.pem ../../build_signed/firmware.signed
