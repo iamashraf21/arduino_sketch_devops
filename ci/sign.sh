00b5ac6a7267bacfacabea5e247ed38cb44527b7
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-19 12:36:32 +0530

    Update sign.sh

--- ci/sign.sh
+++ ci/sign.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 pwd
-cd ../build/Seeeduino.samd.zero/
+ls ../build/
 mkdir ../../build_signed
 python ../../ci/sign.py ../../ci/private.pem firmware.ino.bin 1 --out ../../build_signed/firmware.signed 
 python ../../ci/verify.py ../../ci/public.pem ../../build_signed/firmware.signed
