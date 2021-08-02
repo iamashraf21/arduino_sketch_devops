196de0e0e1d2878a5c30418cdf3645c40718d1bf
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-08-02 13:17:45 +0530

    Update sign.sh

--- ci/sign.sh
+++ ci/sign.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 pwd
-cd ../build/Moteino.samd.moteino_m0
+cd firmware/build/Moteino.samd.moteino_m0
 ls -l
 mkdir ../../build_signed
-python ../../ci/sign.py ../../ci/private.pem firmware.ino.bin 1 --out ../../build_signed/firmware.signed 
-python ../../ci/verify.py ../../ci/public.pem ../../build_signed/firmware.signed
+python ../../../ci/sign.py ../../../ci/private.pem firmware.ino.bin 1 --out ../../build_signed/firmware.signed 
+python ../../../ci/verify.py ../../../ci/public.pem ../../build_signed/firmware.signed
